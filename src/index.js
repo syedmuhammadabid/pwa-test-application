c5ca80c3f39f54008211f090233a00984906668e
Abid Hussain <abid.hussain@contour-software.com>
2022-01-14 18:42:39 +0500

    feat: PWA files added

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { pwaTrackingListeners } from './pwaEventListeners';
-import { compareAppVersion } from './util';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { pwaTrackingListeners } from "./pwaEventListeners";
+import { compareAppVersion } from "./util";
 
 const isBrowser = typeof window !== "undefined";
 
@@ -15,23 +15,28 @@ if (isBrowser) {
 compareAppVersion();
 
 if (isBrowser && "serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-      navigator.serviceWorker
-          .register("/service-worker.js")
-          .then(() => {
-              console.log("Service worker registered");
-          })
-          .catch((err) => {
-              console.log("Service worker registration failed", err);
-          });
-  });
+    navigator.serviceWorker.getRegistrations().then(function (registrations) {
+        for (let registration of registrations) {
+            registration.update();
+        }
+    });
+    window.addEventListener("load", () => {
+        navigator.serviceWorker
+            .register("/service-worker.js")
+            .then(() => {
+                console.log("Service worker registered");
+            })
+            .catch((err) => {
+                console.log("Service worker registration failed", err);
+            });
+    });
 }
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
