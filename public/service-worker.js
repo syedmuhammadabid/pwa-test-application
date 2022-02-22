916d9020a85352d48cfed3cbc4ca968e2c617cf8
Abid Hussain <abid.hussain@contour-software.com>
2022-02-22 14:37:42 +0500

    Service worker change

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,5 +1,42 @@
 self.skipWaiting();
 
-self.addEventListener("fetch", function (_event) {
-  //nothing for now
+const staticCacheName = `site-static-v${
+    Math.floor(Math.random() * 10) + Math.floor(Math.random() * 10)
+}`;
+const assets = ["/", "/index.html", "/logo192.png", "/logo512.png"];
+
+// install event
+self.addEventListener("install", (evt) => {
+    //console.log('service worker installed');
+    evt.waitUntil(
+        caches.open(staticCacheName).then((cache) => {
+            console.log("caching shell assets");
+            cache.addAll(assets);
+        })
+    );
+});
+
+// activate event
+self.addEventListener("activate", (evt) => {
+    //console.log('service worker activated');
+    evt.waitUntil(
+        caches.keys().then((keys) => {
+            //console.log(keys);
+            return Promise.all(
+                keys
+                    .filter((key) => key !== staticCacheName)
+                    .map((key) => caches.delete(key))
+            );
+        })
+    );
+});
+
+// fetch event
+self.addEventListener("fetch", (evt) => {
+    //console.log('fetch event', evt);
+    evt.respondWith(
+        caches.match(evt.request).then((cacheRes) => {
+            return cacheRes || fetch(evt.request);
+        })
+    );
 });
