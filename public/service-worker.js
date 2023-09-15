6a8f65440aa584233ecd46f1776ff4e5b715d804
Syed Muhammad Abid <syedmuhammadabid110@gmail.com>
2023-09-15 09:08:05 +0000

    feat: Commit from codesandbox

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,49 +1,50 @@
+import { version } from "./package.json";
 // self.skipWaiting();
 
 // const staticCacheName = `site-static-v${
-//     Math.floor(Math.random() * 100) + Math.floor(Math.random() * 100)
+//   Math.floor(Math.random() * 100) + Math.floor(Math.random() * 100)
 // }`;
-const staticCacheName = `site-static-v${process.env.REACT_APP_VERSION}`;
+const staticCacheName = `site-static-v${version}`;
 const assets = ["/logo192.png", "/logo512.png"];
 
 // install event
 self.addEventListener("install", (evt) => {
-    console.log("service worker installed");
-    evt.waitUntil(
-        caches.open(staticCacheName).then((cache) => {
-            console.log("caching shell assets");
-            cache.addAll(assets);
-        })
-    );
+  console.log("service worker installed");
+  evt.waitUntil(
+    caches.open(staticCacheName).then((cache) => {
+      console.log("caching shell assets");
+      cache.addAll(assets);
+    }),
+  );
 });
 
 // activate event
 self.addEventListener("activate", (evt) => {
-    console.log("service worker activated");
-    evt.waitUntil(
-        caches.keys().then((keys) => {
-            //console.log(keys);
-            return Promise.all(
-                keys
-                    .filter((key) => key !== staticCacheName)
-                    .map((key) => caches.delete(key))
-            );
-        })
-    );
+  console.log("service worker activated");
+  evt.waitUntil(
+    caches.keys().then((keys) => {
+      //console.log(keys);
+      return Promise.all(
+        keys
+          .filter((key) => key !== staticCacheName)
+          .map((key) => caches.delete(key)),
+      );
+    }),
+  );
 });
 
 // fetch event
 self.addEventListener("fetch", (evt) => {
-    //console.log('fetch event', evt);
-    evt.respondWith(
-        caches.match(evt.request).then((cacheRes) => {
-            return cacheRes || fetch(evt.request);
-        })
-    );
+  //console.log('fetch event', evt);
+  evt.respondWith(
+    caches.match(evt.request).then((cacheRes) => {
+      return cacheRes || fetch(evt.request);
+    }),
+  );
 });
 
 self.addEventListener("message", function (event) {
-    if (event.data.action === "skipWaiting") {
-        self.skipWaiting();
-    }
+  if (event.data.action === "skipWaiting") {
+    self.skipWaiting();
+  }
 });
--- src/App.js
+++ src/App.js
@@ -6,10 +6,6 @@ function App() {
             <header className="App-header">
                 <h1>PWA Test Application</h1>
                 <p>App Version: v{localStorage.getItem("AppVersion")}</p>
-                <div id="snackbar">
-                    A new version of this app is available. Click{" "}
-                    <a id="reload">here</a> to update.
-                </div>
             </header>
         </div>
     );
